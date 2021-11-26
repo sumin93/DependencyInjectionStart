2b3884d10119ac03d54d5209a6cf5caa13d80266
a.sumin <sumin93@mail.ru>
2021-11-26 23:52:58 +0300

    Use Provider instead of just ViewModel

--- app/src/main/java/com/example/dependencyinjectionstart/example2/di/ApplicationComponent.kt
+++ app/src/main/java/com/example/dependencyinjectionstart/example2/di/ApplicationComponent.kt
@@ -2,6 +2,7 @@ package com.example.dependencyinjectionstart.example2.di
 
 import android.content.Context
 import com.example.dependencyinjectionstart.example2.presentation.MainActivity
+import com.example.dependencyinjectionstart.example2.presentation.MainActivity2
 import dagger.BindsInstance
 import dagger.Component
 
@@ -10,6 +11,8 @@ import dagger.Component
 interface ApplicationComponent {
 
     fun inject(activity: MainActivity)
+
+    fun inject(activity: MainActivity2)
 
     @Component.Factory
     interface ApplicationComponentFactory {
--- app/src/main/java/com/example/dependencyinjectionstart/example2/presentation/MainActivity2.kt
+++ app/src/main/java/com/example/dependencyinjectionstart/example2/presentation/MainActivity2.kt
@@ -1,15 +1,13 @@
 package com.example.dependencyinjectionstart.example2.presentation
 
-import android.content.Intent
 import android.os.Bundle
-import android.widget.TextView
 import androidx.appcompat.app.AppCompatActivity
 import androidx.lifecycle.ViewModelProvider
 import com.example.dependencyinjectionstart.R
 import com.example.dependencyinjectionstart.example2.ExampleApp
 import javax.inject.Inject
 
-class MainActivity : AppCompatActivity() {
+class MainActivity2 : AppCompatActivity() {
 
     @Inject
     lateinit var viewModelFactory: ViewModelFactory
@@ -32,10 +30,5 @@ class MainActivity : AppCompatActivity() {
         setContentView(R.layout.activity_main)
         viewModel.method()
         viewModel2.method()
-        findViewById<TextView>(R.id.tv_hello_world).setOnClickListener {
-            Intent(this, MainActivity2::class.java).apply {
-                startActivity(this)
-            }
-        }
     }
 }
