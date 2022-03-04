26423bba0c444fed3ab242df1383a5ea6ef20379
Cavc13 <13liver@mail.ru>
2022-03-04 12:57:13 +0300

    Inject ViewModel

--- app/src/main/java/com/example/dependencyinjectionstart/example2/di/ApplicationComponent.kt
+++ app/src/main/java/com/example/dependencyinjectionstart/example2/di/ApplicationComponent.kt
@@ -10,7 +10,7 @@ import dagger.Component
 import javax.inject.Singleton
 
 @ApplicationScope
-@Component(modules = [DataModule::class, DomainModule::class])
+@Component(modules = [DataModule::class, DomainModule::class, ViewModelModule::class])
 interface ApplicationComponent {
 
     fun inject(activity: MainActivity)
